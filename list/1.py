466d46228f830051623cad0e9e421a5d62523be0
talant <talantbek.karmyshakov@capgemini.com>
2023-01-30 00:02:22 -0600

    recalll python basics

---
+++ list/1.py
@@ -0,0 +1,24 @@
+list1 = ['Naryn', 'Jalalabad', 'Osh', 'Talas', 'Jalalabad', 'Batken']
+name2 = 'talant'
+
+# print all list
+print(list1)
+
+# print starting with index 2
+print(list1[2:])
+
+# print starting at 2 till 4 (4 not included)
+print(list1[2:4])
+
+# print Types
+print(type(name2))
+print(type(list1))
+
+# change values in the list
+list1[2] = 'AtBashy'
+list1[4] = 'Kochkor'
+print(list1)
+
+# Negative indexes gets from the last of the indexing
+print(list1[-4])
+

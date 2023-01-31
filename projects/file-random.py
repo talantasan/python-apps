47c18f8f0881e0fdfef8856dc4abd18514938f62
talant <talant.py@gmail.com>
2023-01-31 03:15:20 +0000

    project random numbers to file ended

--- projects/file-random.py
+++ projects/file-random.py
@@ -1,4 +1,20 @@
 import random
 
 def main():
+    random_numbers_file()
     
+def myrandom():
+    mylist = []
+    for i in range(1, 11):
+        x = random.randint(1, 20)
+        mylist.append(x)
+    return mylist
+
+def random_numbers_file():
+    new_list = myrandom()
+    newfile = open('randomnumbers.txt', 'w')
+    for i in new_list:
+        newfile.write(str(i)+'\n')
+    newfile.close()
+    
+main()

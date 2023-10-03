2d8e76368608240ab161908aa26cbb2251637f31
TalantbekK-mc94119_mcdcorp <talantbek.karmyshakov@capgemini.com>
2023-10-03 13:16:46 -0500

    added

--- classes/1.py
+++ classes/1.py
@@ -1,18 +1,24 @@
-class Customers:
+class Employee:
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = first + '.' + last + '@talantzon.com'
-
-    def fullname(self):
-        return f'{self.first} {self.last}'
+        self.email = first + '.' + last + '@gmail.com'
     
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
 
-cust1 = Customers('Talant', 'Karmyshakov', 5000)
-cust2 = Customers('Samara', 'Ataeva', 6000)
 
-print(cust1.email)
-print(cust2.first)
-print(cust1.fullname())
+emp1 = Employee('talant', 'karmyshakov', 3000)
+emp2 = Employee('samara', 'ataeva', 4000)
 
+# print(emp1.email)
+# print(emp2.email)
+
+# print('{} {}'.format(emp1.first, emp1.last))
+# created new method for displaying fullname, we can call by:
+
+print(emp1.fullname()) 
+
+# we also can pass by Classname
+print(Employee.fullname(emp2))
